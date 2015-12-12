98e63bbdd16e1a7885d7fa4aa43d1244e40475f8
Insaf <diyago@yandex.ru>
2015-12-12 15:27:49 +0300

    added test for final report (single thread, unbounded)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,155 @@
 import ru.sbt.mipt.fifo.SingeThreadQueue;
+import ru.sbt.mipt.fifo.UnboundedQueueImpl;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Insaf on 07.11.2015.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        int numberOfAdds = 1000000;
+        //####    TEST SINGLE THREAD QUERY    ####\\\
 
         SingeThreadQueue singeThreadQueue = new SingeThreadQueue();
 
-        //Using the add method to add items.
-        //Should anything go wrong an exception will be thrown.
-        singeThreadQueue.add("item1");
-        singeThreadQueue.add("item2");
-        singeThreadQueue.add("item3");
+        // measuring adding time
+        long startTime = System.nanoTime();
+
+        for (int i = 0; i < numberOfAdds; i++) {
+            singeThreadQueue.add(5555555.55);
+        }
+        System.out.println("SINGLE THREAD QUERY adding time: " + (System.nanoTime() - startTime) / Math.pow(10, 9));
+
+        // measuring polling time
+        startTime = System.nanoTime();
+        for (int i = 0; i < numberOfAdds; i++) {
+            singeThreadQueue.poll();
+        }
+
+        System.out.println("SINGLE THREAD QUERY polling time: " + (System.nanoTime() - startTime) / Math.pow(10, 9));
+
+        // checking under pressure
+        singeThreadQueue = new SingeThreadQueue();
+
+        // measuring adding time
+        startTime = System.nanoTime();
+
+        for (int i = 0; i < numberOfAdds; i++) {
+            singeThreadQueue.add(heaveCalcFunc(5555555.55));
+        }
+        System.out.println("SINGLE THREAD QUERY adding time under pressure: " + (System.nanoTime() - startTime) / Math.pow(10, 9));
+
+        // measuring polling time
+        startTime = System.nanoTime();
+        for (int i = 0; i < numberOfAdds; i++) {
+            singeThreadQueue.poll();
+        }
+        System.out.println("SINGLE THREAD QUERY polling time under pressure: " + (System.nanoTime() - startTime) / Math.pow(10, 9));
 
 
-        //Removing the first item from the queue.
-        System.out.println("remove: " + singeThreadQueue.poll());
+        //####    TEST MULTI THREAD UNBOUNDED QUERY    ####\\\
+        java.util.Queue<Double> queue = new UnboundedQueueImpl<Double>();
+        int numberOfThread = 4;
+        int targetNumber = numberOfAdds / numberOfThread;
 
-        //Checking what item is first in line without removing it
-        System.out.println("element: " + singeThreadQueue.element());
+        List<Thread> threads = new ArrayList<Thread>();
+        startTime = System.nanoTime();
 
-        //Removing the first item from the queue.
-        System.out.println("poll: " + singeThreadQueue.poll());
+        for (int i = 0; i < numberOfThread; i++) {
+            Thread addingThread;
+            addingThread = new Thread(() -> {
+                for (int j = 0; j < targetNumber; j++) {
+                    queue.add(5555555.55);
+                }
+            });
+            threads.add(addingThread);
+        }
 
-        //Checking what item is first in line without removing it
-        //If the queue is empty a null value will be returned.
-        System.out.println("peek: " + singeThreadQueue.element());
+        threads.forEach(Thread::start);
 
-        System.out.println("poll: " + singeThreadQueue.poll());
-        System.out.println("poll: " + singeThreadQueue.poll());
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        System.out.println("MULTI THREAD (" + numberOfThread + ") QUERY adding time: " + (System.nanoTime() - startTime) / Math.pow(10, 9));
+        threads.clear();
+        startTime = System.nanoTime();
+
+        for (int i = 0; i < numberOfThread; i++) {
+            Thread addingThread;
+            addingThread = new Thread(() -> {
+                for (int j = 0; j < targetNumber; j++) {
+                    queue.poll();
+                }
+            });
+            threads.add(addingThread);
+        }
+
+        threads.forEach(Thread::start);
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        System.out.println("MULTI THREAD (" + numberOfThread + ") QUERY polling time: " + (System.nanoTime() - startTime) / Math.pow(10, 9));
+
+        // under pressure
+
+        threads.clear();
+        startTime = System.nanoTime();
+
+        for (int i = 0; i < numberOfThread; i++) {
+            Thread addingThread;
+            addingThread = new Thread(() -> {
+                for (int j = 0; j < targetNumber; j++) {
+                    queue.add(heaveCalcFunc(5555555.55));
+                }
+            });
+            threads.add(addingThread);
+        }
+
+        threads.forEach(Thread::start);
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        System.out.println("MULTI THREAD (" + numberOfThread + ") QUERY adding time under pressure: " + (System.nanoTime() - startTime) / Math.pow(10, 9));
+        threads.clear();
+        startTime = System.nanoTime();
+
+        for (int i = 0; i < numberOfThread; i++) {
+            Thread addingThread;
+            addingThread = new Thread(() -> {
+                for (int j = 0; j < targetNumber; j++) {
+                    queue.poll();
+                }
+            });
+            threads.add(addingThread);
+        }
+
+        threads.forEach(Thread::start);
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        System.out.println("MULTI THREAD (" + numberOfThread + ") QUERY polling time under pressure: " + (System.nanoTime() - startTime) / Math.pow(10, 9));
 
         return;
     }
+
+    static double heaveCalcFunc(double cur_num) {
+        return Math.pow(cur_num, 0.567) / 2;
+    }
 }
+
+/*
+SINGLE THREAD QUERY adding time: 1.03313518
+SINGLE THREAD QUERY polling time: 0.024947099
+SINGLE THREAD QUERY adding time under pressure: 0.133770534
+SINGLE THREAD QUERY polling time under pressure: 0.012594972
+MULTI THREAD (4) QUERY adding time: 0.852639727
+MULTI THREAD (4) QUERY polling time: 0.2392535
+MULTI THREAD (4) QUERY adding time under pressure: 0.815223576
+MULTI THREAD (4) QUERY polling time under pressure: 0.017239012
+ */
--- src/main/test/java/ru/sbt/mipt/fifo/SingeThreadQueueTest.java
+++ src/main/test/java/ru/sbt/mipt/fifo/SingeThreadQueueTest.java
@@ -2,7 +2,7 @@ package ru.sbt.mipt.fifo;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+    import static org.junit.Assert.*;
 
 /**
  * Created by Insaf on 06.12.2015.
